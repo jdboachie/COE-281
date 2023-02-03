625d784515cfa24f41575e3c554ad9a825ea5888
Jude Boachie <judeboachiekwame@gmail.com>
2023-02-03 09:21:01 +0000

    completed task: working with variables

--- myFirstProgram/main.c
+++ myFirstProgram/main.c
@@ -5,13 +5,23 @@
  * Student Number: 7095021
  * @brief This is my first program in C
  * @date 2023-02-03
- * @version 0.1
 */
+
 #include <stdio.h>
 
+/**
+ * main - Entry point
+ * Return: Always 0 (Success)
+*/
 int main()
 {
+    int age;
+
+    age = 20;
+
     // Print "Hello, World!" to the console
-    print("Hello, World!\n");
+    printf("Hello, World!\nI'm Jude Boachie\nI'm %d years old\n", age);
+
+    // Return 0 to the operating system
     return (0);
 }
