ddd606369650cd6ac544f4a6307a236af47fa7de
Jude Boachie <judeboachiekwame@gmail.com>
2023-02-03 09:00:06 +0000

    Add myFirstProgram via upload

---
+++ myFirstProgram/main.c
@@ -0,0 +1,17 @@
+/**
+ * @file main.c
+ * Name: myFirstProgram
+ * Author: Jude Boachie
+ * Student Number: 7095021
+ * @brief This is my first program in C
+ * @date 2023-02-03
+ * @version 0.1
+*/
+#include <stdio.h>
+
+int main()
+{
+    // Print "Hello, World!" to the console
+    print("Hello, World!\n");
+    return (0);
+}
