f74009fd1fe9128dffb71522c2d71de779696f81
jdboachie <judeboachiekwame@gmail.com>
2023-04-12 17:25:05 +0000

    fix: correct code syntax

--- lab-3/main.c
+++ lab-3/main.c
@@ -3,34 +3,34 @@
 
 int main()
 {
-	// printf("Hello, User!\n");
+	printf("Hello, User!\n");
 
-	// // Ask user to enter name
-	// char name[20];
-	// printf("Enter your name: ");
-	// scanf("%s", name);	// scanf stops reading when it encounters a space
+	// Ask user to enter name
+	char name[20];
+	printf("Enter your name: ");
+	scanf("%s", name);	// scanf stops reading when it encounters a space
 
-	// // Ask user to enter weight
-	// char weight[20];
-	// printf("Enter your weight in kg: ");
-	// scanf("%s", weight);
+	// Ask user to enter weight
+	char weight[20];
+	printf("Enter your weight in kg: ");
+	scanf("%s", weight);
 
-	// // Ask user to enter height
-	// char height [20];
-	// printf("Enter your height in m:  ");
-	// scanf("%s", height);
+	// Ask user to enter height
+	char height [20];
+	printf("Enter your height in m:  ");
+	scanf("%s", height);
 
-	// // Display the values the user entered using puts
-	// puts(weight);
-	// puts(height);
+	// Display the values the user entered using puts
+	puts(weight);
+	puts(height);
 
-	// // Calculate their BMI and display it
-	// float bmi = atoi(weight) / (atoi(height) * atoi(height));
-	// printf("%s, your BMI is %f\n", name, bmi);
+	// Calculate their BMI and display it
+	float bmi = atof(weight) / (atof(height) * atof(height));
+	printf("%s, your BMI is %f\n", name, bmi);
 
 	// Receive a character from the user using getchar
-	// printf("[INFO] BMI calculation complete. Press any key to continue \n");
-	// printf("%c\n", getchar());
+	printf("[INFO] BMI calculation complete. Press any key to continue \n");
+	printf("%c\n", getchar());
 
 	// Display the character using putchar
 	char a = getchar();
